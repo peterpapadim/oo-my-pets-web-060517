b70fd6a49b29c7f99b5413ea03c68a5699d9d33d
Peter <ppapadim12@gmail.com>
2017-06-12 11:00:02 -0400

    done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,15 @@
 class Fish
-  # code goes here
+
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def mood=(fish_mood)
+    @mood = fish_mood
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,66 @@
 class Owner
-  # code goes here
+
+  attr_reader :species
+  attr_accessor :name, :pets
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.size
+  end
+
+  def species
+    @species
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def buy_fish(fish_name)
+    self.pets[:fishes] << Fish.new(fish_name)
+  end
+
+  def buy_cat(cat_name)
+    self.pets[:cats] << Cat.new(cat_name)
+  end
+
+  def buy_dog(dog_name)
+    self.pets[:dogs] << Dog.new(dog_name)
+  end
+
+  def walk_dogs
+    self.pets[:dogs].each { |each_dog| each_dog.mood = "happy" }
+  end
+
+  def play_with_cats
+    self.pets[:cats].each { |each_cat| each_cat.mood = "happy" }
+  end
+
+  def feed_fish
+    self.pets[:fishes].each { |each_fish| each_fish.mood = "happy" }
+  end
+
+  def sell_pets
+    self.pets.values.flatten.each { |each_pet| each_pet.mood = "nervous" }
+    self.pets.each { |pet_type, pets| self.pets[pet_type].clear }
+  end
+
+  def list_pets
+    "I have #{self.pets[:fishes].size} fish, #{self.pets[:dogs].size} dog(s), and #{self.pets[:cats].size} cat(s)."
+  end
+
 end
